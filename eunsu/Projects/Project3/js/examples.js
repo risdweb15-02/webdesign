708f8c6d5a9e7740e4126551b5f23a5ca434e5ae
southsky20 <echang@risd.edu>
2015-02-03 14:15:24 -0500

    change

--- eunsu/Projects/Project3/js/examples.js
+++ eunsu/Projects/Project3/js/examples.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	$("#demosMenu").change(function(){
 	  window.location.href = $(this).find("option:selected").attr("id") + '.html';
 	});
-    
+
+
     $('.pull-me').click(function(){
         $('.panel').slideToggle('slow');
     });
