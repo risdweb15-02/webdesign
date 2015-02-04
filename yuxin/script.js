6efa856a3bfaf29d8dc4d512f798be756e876935
yhan02 <yhan02@risd.edu>
2015-02-04 11:41:30 -0500

    update
    update

---
+++ yuxin/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() { 
+  // jQu$(document).ready(function()ery code 
+  $("ol").css("background-color","yellow");  
+
+  
+
+  $(".frist").click(function() {
+	$(".second").css("background-color","red");
+}); 
+
+
+
+
+});
