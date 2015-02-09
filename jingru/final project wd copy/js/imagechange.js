f842e5e931436619b09e718f4f5ef45c5a7ccb84
gkadray <jguo01@risd.edu>
2015-02-09 08:56:25 +0800

    -
    -

--- jingru/final project wd copy/js/imagechange.js
+++
@@ -1,7 +0,0 @@
-var main=function(){
-	$(".artist").click(function(){
-		$(".imagebox img").attr("src","img/wolfgang/bk2.jpg");
-	})
-}
-
-$(document).ready(main)
--- jingru/final project wd copy/js/scrollheader.js
+++
@@ -1,8 +0,0 @@
-$(window).on("scroll", function () {
-    if ($(this).scrollTop() > 100) {
-        $("#header").addClass("not-transparent");
-    }
-    else {
-        $("#header").removeClass("not-transparent");
-    }
-});
